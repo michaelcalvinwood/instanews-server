71c6f64c048fb9ee3ac079f5677a5756be30d1c9
Michael Wood <michaelwood33311@icloud.com>
2023-05-22 14:37:18 -0400

    added extracting text from article HTML

--- utils/url.js
+++ utils/url.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
-const articleExtractor = require('@extractus/article-extractor')
+const articleExtractor = require('@extractus/article-extractor');
+const { convert } = require('html-to-text');
 
 //const url = 'https://www.pymnts.com/news/retail/2023/will-consumers-pay-50-for-drugstore-brand-sunscreen/';
 
@@ -13,6 +14,14 @@ exports.articleExtractor = async (url, html = false) => {
         return false;
       }
 
+    const options = {
+        selectors: [
+          { selector: 'a', options: { ignoreHref: true } },
+          { selector: 'a.button', format: 'skip' }
+        ]
+      }
+    const text = convert(article.content, options);
+    console.log(text);
     return article;
 }
 

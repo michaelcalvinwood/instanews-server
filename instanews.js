3df7b64e110636d7ef23e0e4d247e9e787965344
Michael Wood <michaelwood33311@icloud.com>
2023-05-31 08:04:32 -0400

    added mysql connectivity

--- instanews.js
+++ instanews.js
@@ -14,6 +14,43 @@ const nlp = require('./utils/nlp');
 const serp = require('./utils/serpWow');
 const wp = require('./utils/wordpress');
 
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    connectionLimit : 2, //important
+    host     : process.env.MYSQL_HOST,
+    user     : process.env.MYSQL_USER,
+    password : process.env.MYSQL_PASSWORD,
+    database : process.env.MYSQL_DATABASE,
+    debug    :  false
+});
+
+let databaseReady = false;
+
+pool.query("SHOW DATABASES",(err, data) => {
+    if(err) {
+        console.error(err);
+        return;
+    }
+    // rows fetch
+    console.log(data);
+    databaseReady = true;
+});
+
+const mysqlQuery = query => {
+  return new Promise ((resolve, reject) => {
+    pool.query(query,(err, data) => {
+      if(err) {
+          console.error(err);
+          return reject(err);
+      }
+      // rows fetch
+      //console.log(data);
+      return resolve(data);
+  });
+  })
+}
+
 const app = express();
 app.use(express.static('public'));
 app.use(express.json({limit: '200mb'})); 
